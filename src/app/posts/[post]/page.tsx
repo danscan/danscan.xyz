3e41988c8d76e34a23e2bb4023a8248e3357d770
danscan <dan@dscanlon.com>
2023-05-01 11:07:33 -0400

    fix(layout.tsx): update email link to use correct email address feat(page.tsx): add function to generate static params for posts and use it to get list of mdx filenames in src/posts directory

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -3,6 +3,27 @@ import type { SerializeOptions } from 'next-mdx-remote/dist/types';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import rehypePrettyCode from 'rehype-pretty-code';
 
+export async function generateStaticParams() {
+  const posts = await getMdxFilenames('src/posts');
+
+  return posts.map((post) => ({
+    post,
+  }));
+}
+
+async function getMdxFilenames(directory: string): Promise<string[]> {
+  try {
+    const filenames = await fs.readdir(directory);
+    const mdxFilenames = filenames
+      .filter((filename) => filename.endsWith('.mdx'))
+      .map((filename) => filename.slice(0, -4));
+    return mdxFilenames;
+  } catch (error) {
+    console.error('Error reading directory:', error);
+    return [];
+  }
+}
+
 export default async function PostPage({
   params,
 }: {
